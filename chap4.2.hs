75b17264130d31b65b171b5df43f1038002670d1
Yi Lu <luyi0619@outlook.com>
2015-07-23 18:27:51 +0800

    chap4.2 1-5 Done!

--- chap4.2.hs
+++ chap4.2.hs
@@ -28,8 +28,37 @@ asInt_either xs = foldl step (Right 0) xs
 
 -- 4.2.3 The Prelude function concat concatenates a list of lists into a single list, and has the following type. Write your own definition of concat using foldr.
 
+concat' :: [[a]] -> [a]
+concat' = foldr (++) [] 
+
 -- 4.2.4 Write your own definition of the standard takeWhile function, first using explicit recursion, then foldr.
+
+takeWhile' f [] = []
+takeWhile' f (x:xs) 
+            | f x = x : (takeWhile' f xs)
+            | otherwise = []
+            
+takeWhile'' f = foldr step []
+        where step x acc 
+                | f x = x : acc
+                | otherwise = []
+                
 
 -- 4.2.5 The Data.List module defines a function, groupBy, which has the following type. Use ghci to load the Data.List module and figure out what groupBy does, then write your own implementation using a fold.
 
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+
+groupBy' f = foldr step []
+        where step x acc 
+                |  null acc = [x] : acc
+                |  otherwise = let lastg = head acc
+                               in  let lastx = head lastg
+                                    in if f x lastx then (x : lastg) : (tail acc) else [x] : acc 
+
+
 -- 4.2.6 How many of the following Prelude functions can you rewrite using list folds? any, cycle, words, unlines. For those functions where you can use either foldl' or foldr, which is more appropriate in each case?
+
+
+any' f = foldl (\acc x -> acc || (f x) ) False 
+
+
