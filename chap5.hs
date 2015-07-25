bc09c7093b70c5fa588e0a1f8fd55feb8b776fc7
Yi Lu <luyi0619@outlook.com>
2015-07-25 21:51:12 +0800

    Prepare 5 & 6

---
+++ chap5.hs
@@ -0,0 +1,3 @@
+-- 5.1 Write a function, fill, with the following type signature. It should add spaces to a document until it is the given number of columns wide. If it is already wider than this value, it should add no spaces.
+
+-- 5.2 Our pretty printer does not take nesting into account. Whenever we open parentheses, braces, or brackets, any lines that follow should be indented so that they are aligned with the opening character until a matching closing character is encountered. Add support for nesting, with a controllable amount of indentation.
---
+++ chap6.hs
@@ -0,0 +1,3 @@
+-- 6.1 Load the Control.Arrow module into ghci, and find out what the second function does.
+
+-- 6.2 What is the type of (,)? When you use it in ghci, what does it do? What about (,,)?
