0af2a0c106e4541c29381a6eef6ea51fc732bf56
Yi Lu <luyi0619@outlook.com>
2015-07-25 21:44:10 +0800

    chap3.2 done

--- chap3.2.hs
+++ chap3.2.hs
@@ -1,4 +1,5 @@
 import Data.List (sortBy)
+import Prelude hiding (Right, Left)
 
 -- 3.2.1 Write a function that computes the number of elements in a list. To test it, ensure that it gives the same answers as the standard length function. 
 
@@ -50,15 +51,41 @@ height (Node a left right) = 1 + max (height left) (height right)
 data Direction = Right
                | Left
                | Line
-                 deriving (Show)
+                 deriving (Show, Eq)
 
 -- 3.2.10 Write a function that calculates the turn made by three 2D points and returns a Direction.
 
 data Point = Point {
           x :: Double, 
           y :: Double
-        } deriving (Show)
+        } deriving (Show, Eq)
+
+ccw p1 p2 p3 | value > 0 = Left 
+             | value < 0 = Right 
+             | otherwise = Line
+          where value = (x p2 - x p1) * (y p3 - y p1) - (y p2 - y p1) * (x p3 - x p1)
 
 -- 3.2.11 Define a function that takes a list of 2D points and computes the direction of each successive triple. Given a list of points [a,b,c,d,e], it should begin by computing the turn made by [a,b,c], then the turn made by [b,c,d], then [c,d,e]. Your function should return a list of Direction. 1
 
+succ_direction xs | length xs < 3 = error ("length < 3")
+succ_direction [a, b, c] = [ccw a b c]
+succ_direction (a:b:c:xs) = ccw a b c : succ_direction (b:c:xs)
+
+
 -- 3.2.12 Using the code from the preceding three exercises, implement Graham's scan algorithm for the convex hull of a set of 2D points. You can find good description of what a convex hull. is, and how the Graham scan algorithm should work, on Wikipedia.
+
+dist p1 p2 = (x p1 - x p2) ^ 2 + (y p1 - y p2) ^ 2
+
+sortAngle p xs = sortBy comp xs
+     where comp a b
+            | ccw p a b == Right = GT
+            | ccw p a b == Left  = LT
+            | otherwise = compare (dist p a) (dist p b)
+
+graham_scan xs | length xs < 3 = error ("lenght < 3")
+graham_scan xs = scan (tail rem) [head rem, p]
+                 where p = foldl (\acc e -> if y e < y acc || (y e == y acc && x e < x acc) then e else acc) (head xs) (tail xs) 
+                       rem = (sortAngle p . filter (/= p)) xs
+                       scan [] acc = acc
+                       scan now@(x:xs) acc@(x':x'': _) | ccw x'' x' x /= Right = scan xs (x:acc)
+                                                       | otherwise = scan now (tail acc)
