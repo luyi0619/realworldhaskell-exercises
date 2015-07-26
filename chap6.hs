9150a651db8842e1e7fcb3d3426a420774176f50
Yi Lu <luyi0619@outlook.com>
2015-07-26 23:14:16 +0800

    6.1

--- chap6.hs
+++ chap6.hs
@@ -1,3 +1,5 @@
 -- 6.1 Load the Control.Arrow module into ghci, and find out what the second function does.
 
+Create a tuple and a triple.
+
 -- 6.2 What is the type of (,)? When you use it in ghci, what does it do? What about (,,)?
