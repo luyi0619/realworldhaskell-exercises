f2cd5d3e4bde194139f0aedf77006a1b75b7f4db
Yi Lu <luyi0619@outlook.com>
2015-07-27 23:27:39 +0800

    chap6 done

--- chap6.hs
+++ chap6.hs
@@ -1,5 +1,7 @@
 -- 6.1 Load the Control.Arrow module into ghci, and find out what the second function does.
 
-Create a tuple and a triple.
+Apply the function on the second element of a tuple.
 
 -- 6.2 What is the type of (,)? When you use it in ghci, what does it do? What about (,,)?
+
+Create a tuple and a triple.
